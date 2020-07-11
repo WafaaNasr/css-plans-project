1ebe89841c8db087c6cac977cffa2d1cb710fbcc
Wafaa Nasr <wafaa.aanasr@gmail.com>
2020-07-11 19:48:41 +0200

    use js to access modal and mobile-navigation and control their appearnce

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector('.modal__action--negative');
 var planButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileMenu = document.querySelector('.mobile-nav');
 
 planButtons.forEach(button =>
     button.addEventListener('click', () => {
@@ -9,14 +11,28 @@ planButtons.forEach(button =>
     })
 );
 
-backdrop.addEventListener('click', () =>toggleModal(false));
-modalNoButton.addEventListener('click', () => toggleModal(false));
+backdrop.addEventListener('click', () => {
+    mobileMenu.classList.remove('open');
+    toggleModal(false);
+});
+if (modalNoButton)
+    modalNoButton.addEventListener('click', () => toggleModal(false));
 
 
 const toggleModal = show => {
-    if (show)
-        modal.style.display = backdrop.style.display = 'block';
-    else
-        modal.style.display = backdrop.style.display = 'none';
-
+    if (show) {
+        if (modal)
+            modal.classList.add('open');
+        backdrop.classList.add('open');
+    }
+    else {
+        if (modal)
+            modal.classList.remove('open');
+        backdrop.classList.remove('open');
+    }
 }
+
+toggleButton.addEventListener('click', () => {
+    mobileMenu.classList.add('open');
+    backdrop.classList.add('open');
+})
